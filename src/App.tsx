c902aef7ee1f10414795e1ac7be9e3b4881e1b15
mobin afshari <mma137813781378@gmail.com>
2025-01-27 22:37:11 +0330

    feat: fonts added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import '@config/i18n';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <p
+      style={{
+        fontSize: '36px',
+        lineHeight: '52px',
+        fontWeight: 700,
+      }}
+    >
+      تجربه‌ی زندگی راحت‌تر، سریع‌تر و به‌صرفه‌تر با سوپراپلیکیشن اسنپ!
+    </p>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.scss';
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then((registration) => {
-      console.log('Service Worker registered:', registration);
-    })
-    .catch((error) => {
-      console.error('Service Worker registration failed:', error);
-    });
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/sw.js')
+//     .then((registration) => {
+//       console.log('Service Worker registered:', registration);
+//     })
+//     .catch((error) => {
+//       console.error('Service Worker registration failed:', error);
+//     });
+// }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
