8245c7abfd8be27accd6ce93d8e2fbfa08c0a2dd
mobin afshari <mma137813781378@gmail.com>
2025-01-31 19:57:47 +0330

    feat: try to adding mapbox

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useRef } from 'react';
+import Map from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 function Home() {
-  return <div>اسنپ</div>;
+  const mapContainerRef = useRef(null);
+
+  return (
+    <div>
+      <h1>اسنپ</h1>
+      <Map
+        mapboxAccessToken="<Mapbox access token>"
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        style={{ width: 600, height: 400 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
+    </div>
+  );
 }
 
 export default Home;
