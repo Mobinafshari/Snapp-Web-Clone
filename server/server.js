d0dac1c538204ec79eb0ee13d33d79c4eb58ea2a
mobin afshari <mma137813781378@gmail.com>
2025-03-07 20:04:07 +0330

    feat: perometheus and grafana added for server monitoring

--- server/server.js
+++ server/server.js
@@ -2,10 +2,38 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import axios from 'axios';
+import client from 'prom-client';
+
 const app = express();
 app.use(morgan('dev'))
 app.use(helmet())
 const PORT = 8000;
+
+//These are for configurations of Prometheus metric to monitor server details.
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+const requestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status'],
+});
+
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    requestCounter.inc({
+      method: req.method,
+      route: req.path,
+      status: res.statusCode,
+    });
+  });
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 app.get('/server-alive', (req, res) => {
   res.send('server is alive');
