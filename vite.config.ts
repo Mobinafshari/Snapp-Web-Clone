7dc50cea2f95cb1acce73f46085c6ce005a12e29
mobin afshari <mma137813781378@gmail.com>
2025-04-18 11:07:08 +0330

    fix: gonna fix react vendor download

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,9 @@ export default defineConfig({
       output: {
         manualChunks(id: string) {
           if (id.includes('node_modules')) {
-            // if (id.includes('react')) {
-            //   return 'react-vendor';
-            // }
+            if (id.includes('react')) {
+              return 'react-vendor';
+            }
             if (id.includes('lodash')) {
               return 'lodash-vendor';
             }
